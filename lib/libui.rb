3cbe3c6edda52b146fe551395a9f3c979f7f1a7d
kojix2 <2xijok@gmail.com>
2020-12-12 19:17:29 +0900

    Allow shared library locations to be specified with the LIBUIDIR environment variable

--- lib/libui.rb
+++ lib/libui.rb
@@ -9,16 +9,19 @@ module LibUI
     attr_accessor :ffi_lib
   end
 
-  self.ffi_lib = case RbConfig::CONFIG['host_os']
-                 when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
-                   # File.expand_path("libui.dll", ENV['LIBUIDIR'])
-                   File.expand_path('../vendor/libui.dll', __dir__)
-                 when /darwin|mac os/
-                   # File.expand_path("libui.dylib", ENV['LIBUIDIR'])
-                   File.expand_path('../vendor/libui.dylib', __dir__)
-                 else # TODO: Mac
-                   # File.expand_path("libui.so", ENV['LIBUIDIR'])
-                   File.expand_path('../vendor/libui.so', __dir__)
+  lib_name = case RbConfig::CONFIG['host_os']
+             when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+               'libui.dll'
+             when /darwin|mac os/
+               'libui.dylib'
+             else
+               'libui.so'
+             end
+
+  self.ffi_lib = if ENV['LIBUIDIR'] && !ENV['LIBUIDIR'].empty?
+                   File.expand_path(lib_name, ENV['LIBUIDIR'])
+                 else
+                   File.expand_path("../vendor/#{lib_name}", __dir__)
                  end
 
   require_relative 'libui/ffi'
