5810fc11c63cc2f767f3d436c1b67e4d6b75fb10
kojix2 <2xijok@gmail.com>
2020-12-12 18:56:20 +0900

    Leave comments about Fiddle's monkey patch

--- lib/libui.rb
+++ lib/libui.rb
@@ -25,26 +25,37 @@ module LibUI
 
   class << self
     FFI.func_map.keys.each do |original_method_name|
+      # Convert snake_case to CamelCase.
       name = original_method_name.delete_prefix('ui')
                                  .gsub(/::/, '/')
                                  .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
                                  .gsub(/([a-z\d])([A-Z])/, '\1_\2')
                                  .tr('-', '_')
                                  .downcase
+
       func = FFI.func_map[original_method_name]
+
       define_method(name) do |*args, &blk|
+        # Assume that block is the last argument.
         args << blk if blk
+
+        # The proc object is converted to a Closure::BlockCaller object.
         args.map!.with_index do |arg, idx|
           if arg.is_a?(Proc)
+            # The types of the function arguments are recorded beforehand.
+            # See the monkey patch in ffi.rb.
             Fiddle::Closure::BlockCaller.new(*func.inner_functions[idx][1..2], &arg)
           else
             arg
           end
         end
-        siz = func.argtype.size
-        if args.size < siz
-          args[siz-1] = nil
+
+        # Make it possible to omit the last nil. This may be an over-optimization.
+        siz = func.argtype.size - 1
+        if args.size == siz
+          args[siz] = nil
         end
+        
         FFI.public_send(original_method_name, *args)
       end
     end
