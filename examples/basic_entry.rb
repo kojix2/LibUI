4a8010c5cb9677864494a0623f8a8437150c075c
kojix2 <2xijok@gmail.com>
2020-12-24 18:08:05 +0900

    Fix basic_entry example

--- examples/basic_entry.rb
+++ examples/basic_entry.rb
@@ -6,7 +6,7 @@ UI = LibUI
 
 UI.init
 
-main_window = UI.new_window('hello world', 300, 50, 1)
+main_window = UI.new_window('Basic Entry', 300, 50, 1)
 UI.window_on_closing(main_window) do
   puts 'Bye Bye'
   UI.control_destroy(main_window)
@@ -26,7 +26,7 @@ UI.box_append(hbox, entry, 1)
 button = UI.new_button('Button')
 UI.button_on_clicked(button) do
   text = UI.entry_text(entry).to_s
-  UI.msg_box(main_window, '入力した文字は次の通りです', text)
+  UI.msg_box(main_window, 'You entered', text)
   0
 end
 
