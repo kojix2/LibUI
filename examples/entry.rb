5265c2769d259f6c5a53a65a4d0642eea70ea8a0
kojix2 <2xijok@gmail.com>
2020-12-24 17:47:40 +0900

    Add entry example

---
+++ examples/entry.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require 'libui'
+
+UI = LibUI
+
+UI.init
+
+main_window = UI.new_window('hello world', 300, 50, 1)
+UI.window_on_closing(main_window) do
+  puts 'Bye Bye'
+  UI.control_destroy(main_window)
+  UI.quit
+  0
+end
+
+hbox = UI.new_horizontal_box
+UI.window_set_child(main_window, hbox)
+
+entry = UI.new_entry
+UI.entry_on_changed(entry) do
+  puts UI.entry_text(entry).to_s
+end
+UI.box_append(hbox, entry, 1)
+
+button = UI.new_button('Button')
+UI.button_on_clicked(button) do
+  text = UI.entry_text(entry).to_s
+  UI.msg_box(main_window, '入力した文字は次の通りです', text)
+  0
+end
+
+UI.box_append(hbox, button, 0)
+
+UI.control_show(main_window)
+UI.main
+UI.quit
