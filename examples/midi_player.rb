2338d886b72a4ee91257165b07846307b95e5d79
kojix2 <2xijok@gmail.com>
2020-12-26 16:20:54 +0900

    Improved midi_player example

--- examples/midi_player.rb
+++ examples/midi_player.rb
@@ -1,47 +1,48 @@
 # frozen_string_literal: true
 
 require 'libui'
-
-music_directory = File.expand_path(ARGV[0] || '~/Music/')
-midi_files      = Dir.glob(File.join(music_directory, '**/*.mid'))
-                     .sort_by { |path| File.basename(path) }
-VERSION = '0.0.1'
-
-@pid = nil
-
 UI = LibUI
 
-init = UI.init
+class TinyMidiPlayer
+  VERSION = '0.0.1'
 
-play_midi = proc do
-  if @pid.nil? && @selected_file
-    begin
-      @pid = spawn "timidity #{@selected_file}"
-      @th = Process.detach @pid
-    rescue Errno::ENOENT => e
-      warn 'Timidty++ not found. Please install Timidity++.'
-      warn 'https://sourceforge.net/projects/timidity/'
-    end
+  def initialize
+    init = UI.init
+    @pid = nil
+    @music_directory = File.expand_path(ARGV[0] || '~/Music/')
+    @midi_files      = Dir.glob(File.join(@music_directory, '**/*.mid'))
+                          .sort_by { |path| File.basename(path) }
+    at_exit { stop_midi }
+    create_gui
   end
-  0
-end
 
-stop_midi = proc do
-  if @pid
-    if @th.alive?
-      Process.kill(:SIGKILL, @pid)
-      @pid = nil
-    else
-      @pid = nil
+  def stop_midi
+    if @pid
+      if @th.alive?
+        Process.kill(:SIGKILL, @pid)
+        @pid = nil
+      else
+        @pid = nil
+      end
     end
+    0
   end
-end
 
-help_menu = UI.new_menu('Help')
-version_item = UI.menu_append_item(help_menu, 'Version')
+  def play_midi
+    stop_midi
+    if @pid.nil? && @selected_file
+      begin
+        @pid = spawn "timidity #{@selected_file}"
+        @th = Process.detach @pid
+      rescue Errno::ENOENT => e
+        warn 'Timidty++ not found. Please install Timidity++.'
+        warn 'https://sourceforge.net/projects/timidity/'
+      end
+    end
+    0
+  end
 
-UI.new_window('Tiny Midi Player', 200, 50, 1).tap do |main_window|
-  UI.menu_item_on_clicked(version_item) do
+  def show_version(main_window)
     UI.msg_box(main_window,
                'Tiny Midi Player',
                "Written in Ruby\n" \
@@ -50,45 +51,65 @@ UI.new_window('Tiny Midi Player', 200, 50, 1).tap do |main_window|
     0
   end
 
-  UI.window_on_closing(main_window) do
-    UI.control_destroy(main_window)
-    UI.quit
-    0
-  end
+  def create_gui
+    # loop_menu = UI.new_menu('Repeat')
+    # items = %w[Off One].map do |item_name|
+    #   item = UI.menu_append_check_item(loop_menu, item_name)
+    # end
+    # items.each_with_index do |item, idx|
+    #   UI.menu_item_on_clicked(item) do
+    #     @repeat = idx
+    #     (items - [item]).each do |i|
+    #       UI.menu_item_set_checked(i, 0)
+    #     end
+    #     0
+    #   end
+    # end
 
-  UI.new_horizontal_box.tap do |hbox|
-    UI.new_vertical_box.tap do |vbox|
-      UI.new_button('▶').tap do |button1|
-        UI.button_on_clicked(button1, play_midi)
-        UI.box_append(vbox, button1, 1)
-      end
-      UI.new_button('■').tap do |button2|
-        UI.button_on_clicked(button2, stop_midi)
-        at_exit(&stop_midi)
-        UI.box_append(vbox, button2, 1)
-      end
-      UI.box_append(hbox, vbox, 0)
-    end
-    UI.window_set_child(main_window, hbox)
+    help_menu = UI.new_menu('Help')
+    version_item = UI.menu_append_item(help_menu, 'Version')
 
-    UI.new_combobox.tap do |cbox|
-      midi_files.each do |path|
-        name = File.basename(path)
-        UI.combobox_append(cbox, name)
-      end
-      UI.combobox_on_selected(cbox) do |ptr|
-        @selected_file = midi_files[UI.combobox_selected(ptr)]
-        if @th&.alive?
-          stop_midi.call
-          play_midi.call
-        end
+    UI.new_window('Tiny Midi Player', 200, 50, 1).tap do |main_window|
+      UI.menu_item_on_clicked(version_item) { show_version(main_window) }
+
+      UI.window_on_closing(main_window) do
+        UI.control_destroy(main_window)
+        UI.quit
         0
       end
-      UI.box_append(hbox, cbox, 1)
+
+      UI.new_horizontal_box.tap do |hbox|
+        UI.new_vertical_box.tap do |vbox|
+          UI.new_button('▶').tap do |button1|
+            UI.button_on_clicked(button1) { play_midi }
+            UI.box_append(vbox, button1, 1)
+          end
+          UI.new_button('■').tap do |button2|
+            UI.button_on_clicked(button2) { stop_midi }
+            UI.box_append(vbox, button2, 1)
+          end
+          UI.box_append(hbox, vbox, 0)
+        end
+        UI.window_set_child(main_window, hbox)
+
+        UI.new_combobox.tap do |cbox|
+          @midi_files.each do |path|
+            name = File.basename(path)
+            UI.combobox_append(cbox, name)
+          end
+          UI.combobox_on_selected(cbox) do |ptr|
+            @selected_file = @midi_files[UI.combobox_selected(ptr)]
+            play_midi if @th&.alive?
+            0
+          end
+          UI.box_append(hbox, cbox, 1)
+        end
+      end
+      UI.control_show(main_window)
     end
+    UI.main
+    UI.quit
   end
-  UI.control_show(main_window)
 end
 
-UI.main
-UI.quit
+TinyMidiPlayer.new
