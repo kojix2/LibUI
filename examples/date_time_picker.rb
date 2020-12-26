c4112060c5715457f01f6947872eff29880b4ee1
kojix2 <2xijok@gmail.com>
2020-12-26 17:35:12 +0900

    Add date_time_picker example

---
+++ examples/date_time_picker.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'libui'
+
+UI = LibUI
+
+UI.init
+
+vbox = UI.new_vertical_box
+
+date_time_picker = UI.new_date_time_picker
+
+time = UI::FFI::TM.malloc
+
+UI.date_time_picker_on_changed(date_time_picker) do
+  UI.date_time_picker_time(date_time_picker, time)
+  p sec: time.tm_sec,
+    min: time.tm_min,
+    hour: time.tm_hour,
+    mday: time.tm_mday,
+    mon: time.tm_mon,
+    year: time.tm_year,
+    wday: time.tm_wday,
+    yday: time.tm_yday,
+    isdst: time.tm_isdst
+end
+UI.box_append(vbox, date_time_picker, 1)
+
+main_window = UI.new_window('Date Time Pickers', 300, 200, 1)
+UI.window_on_closing(main_window) do
+  puts 'Bye Bye'
+  UI.control_destroy(main_window)
+  UI.quit
+  0
+end
+UI.window_set_child(main_window, vbox)
+UI.control_show(main_window)
+
+UI.main
+UI.quit
