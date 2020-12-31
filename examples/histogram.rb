bb7ce78a4d8c654919e7ecbe59d7b952e5a519ae
kojix2 <2xijok@gmail.com>
2020-12-31 21:22:53 +0900

    Add histogram example

---
+++ examples/histogram.rb
@@ -0,0 +1,185 @@
+# https://github.com/jamescook/libui-ruby/blob/master/example/histogram.rb
+
+require 'libui'
+
+UI = LibUI
+
+X_OFF_LEFT   = 20
+Y_OFF_TOP    = 20
+X_OFF_RIGHT  = 20
+Y_OFF_BOTTOM = 20
+POINT_RADIUS = 5
+
+init         = UI.init
+handler      = UI::FFI::AreaHandler.malloc
+histogram    = UI.new_area(handler)
+brush        = UI::FFI::DrawBrush.malloc
+color_button = UI.new_color_button
+blue         = 0x1E90FF
+datapoints   = []
+
+def graph_size(area_width, area_height)
+  graph_width = area_width - X_OFF_LEFT - X_OFF_RIGHT
+  graph_height = area_height - Y_OFF_TOP - Y_OFF_BOTTOM
+  [graph_width, graph_height]
+end
+
+matrix = UI::FFI::DrawMatrix.malloc
+
+def point_locations(datapoints, width, height)
+  xincr = width / 9.0 # 10 - 1 to make the last point be at the end
+  yincr = height / 100.0
+
+  data = []
+  datapoints.each_with_index do |dp, i|
+    val = 100 - UI.spinbox_value(dp)
+    data << [xincr * i, yincr * val]
+    i += 1
+  end
+
+  data
+end
+
+def construct_graph(datapoints, width, height, should_extend)
+  locations = point_locations(datapoints, width, height)
+  path = UI.draw_new_path(0) # winding
+  first_location = locations[0] # x and y
+  UI.draw_path_new_figure(path, first_location[0], first_location[1])
+  locations.each do |loc|
+    UI.draw_path_line_to(path, loc[0], loc[1])
+  end
+
+  if should_extend
+    UI.draw_path_line_to(path, width, height)
+    UI.draw_path_line_to(path, 0, height)
+    UI.draw_path_close_figure(path)
+  end
+
+  UI.draw_path_end(path)
+
+  path
+end
+
+handler_draw_event = Fiddle::Closure::BlockCaller.new(
+  1, [1, 1, 1]
+) do |_area_handler, _area, area_draw_params|
+  area_draw_params = UI::FFI::AreaDrawParams.new(area_draw_params)
+  path = UI.draw_new_path(0) # winding
+  UI.draw_path_add_rectangle(path, 0, 0, area_draw_params.AreaWidth, area_draw_params.AreaHeight)
+  UI.draw_path_end(path)
+  set_solid_brush(brush, 0xFFFFFF, 1.0) # white
+  UI.draw_fill(area_draw_params.Context, path, brush.to_ptr)
+  UI.draw_free_path(path)
+  dsp = UI::FFI::DrawStrokeParams.malloc
+  dsp.Cap = 0 # flat
+  dsp.Join = 0 # miter
+  dsp.Thickness = 2
+  dsp.MiterLimit = 10 # DEFAULT_MITER_LIMIT
+
+  # draw axes
+  set_solid_brush(brush, 0x000000, 1.0) # black
+  graph_width, graph_height = *graph_size(area_draw_params.AreaWidth, area_draw_params.AreaHeight)
+
+  path = UI.draw_new_path(0) # winding
+  UI.draw_path_new_figure(path, X_OFF_LEFT, Y_OFF_TOP)
+  UI.draw_path_line_to(path, X_OFF_LEFT, Y_OFF_TOP + graph_height)
+  UI.draw_path_end(path)
+  UI.draw_stroke(area_draw_params.Context, path, brush, dsp)
+  UI.draw_free_path(path)
+
+  # now transform the coordinate space so (0, 0) is the top-left corner of the graph
+  UI.draw_matrix_set_identity(matrix)
+  UI.draw_matrix_translate(matrix, X_OFF_LEFT, Y_OFF_TOP)
+  UI.draw_transform(area_draw_params.Context, matrix)
+
+  # now get the color for the graph itself and set up the brush
+  #	uiColorButtonColor(colorButton, &graphR, &graphG, &graphB, &graphA)
+  graph_r = Fiddle::Pointer.malloc(8) # double
+  graph_g = Fiddle::Pointer.malloc(8) # double
+  graph_b = Fiddle::Pointer.malloc(8) # double
+  graph_a = Fiddle::Pointer.malloc(8) # double
+
+  UI.color_button_color(color_button, graph_r, graph_g, graph_b, graph_a)
+  brush.Type = 0 # solid
+  brush.R = graph_r[0, 8].unpack1('d')
+  brush.G = graph_g[0, 8].unpack1('d')
+  brush.B = graph_b[0, 8].unpack1('d')
+
+  # now create the fill for the graph below the graph line
+  path = construct_graph(datapoints, graph_width, graph_height, true)
+  brush.A = graph_a[0, 8].unpack1('d') / 2.0
+  UI.draw_fill(area_draw_params.Context, path, brush)
+  UI.draw_free_path(path)
+
+  # now draw the histogram line
+  path = construct_graph(datapoints, graph_width, graph_height, false)
+  brush.A = graph_a[0, 8].unpack1('d')
+  UI.draw_stroke(area_draw_params.Context, path, brush, dsp)
+  UI.draw_free_path(path)
+  0
+end
+
+handler.Draw         = handler_draw_event
+handler.MouseEvent   = Fiddle::Closure::BlockCaller.new(0, [0]) {}
+handler.MouseCrossed = Fiddle::Closure::BlockCaller.new(0, [0]) {}
+handler.DragBroken   = Fiddle::Closure::BlockCaller.new(0, [0]) {}
+handler.KeyEvent     = Fiddle::Closure::BlockCaller.new(0, [0]) {}
+
+UI.freeInitError(init) unless init.nil?
+
+hbox = UI.new_horizontal_box
+UI.box_set_padded(hbox, 1)
+
+vbox = UI.new_vertical_box
+UI.box_set_padded(vbox, 1)
+UI.box_append(hbox, vbox, 0)
+UI.box_append(hbox, histogram, 1)
+
+spinbox_changed = proc do |_ptr|
+  UI.area_queue_redraw_all(histogram)
+  0
+end
+
+10.times do |i|
+  datapoints << UI.new_spinbox(0, 100)
+  UI.spinbox_set_value(datapoints[i], Random.new.rand(90))
+  UI.spinbox_on_changed(datapoints[i], spinbox_changed, nil)
+  UI.box_append(vbox, datapoints[i], 0)
+end
+
+def set_solid_brush(brush, color, alpha)
+  brush.Type = 0 # solid
+  brush.R = ((color >> 16) & 0xFF) / 255.0
+  brush.G = ((color >> 8) & 0xFF) / 255.0
+  brush.B = (color & 0xFF) / 255.0
+  brush.A = alpha
+  brush
+end
+
+set_solid_brush(brush, blue, 1.0)
+UI.color_button_set_color(color_button, brush.R, brush.G, brush.B, brush.A)
+
+color_changed = proc do |_ptr|
+  UI.area_queue_redraw_all(histogram)
+  0
+end
+
+UI.color_button_on_changed(color_button, color_changed, nil)
+UI.box_append(vbox, color_button, 0)
+
+MAIN_WINDOW = UI.new_window('histogram example', 640, 480, 1)
+UI.window_set_margined(MAIN_WINDOW, 1)
+UI.window_set_child(MAIN_WINDOW, hbox)
+
+should_quit = proc do |_ptr|
+  UI.control_destroy(MAIN_WINDOW)
+  UI.quit
+  0
+end
+
+UI.window_on_closing(MAIN_WINDOW, should_quit, nil)
+UI.on_should_quit(should_quit, nil)
+UI.control_show(MAIN_WINDOW)
+
+UI.main
+UI.quit
