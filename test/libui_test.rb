279c582b11abdb1b01339ecf35eb7a551c8df762
kojix2 <2xijok@gmail.com>
2021-01-03 16:50:08 +0900

    Add basic_window test

--- test/libui_test.rb
+++ test/libui_test.rb
@@ -14,8 +14,24 @@ class LibUITest < Minitest::Test
   end
 
   def test_method_call
-    pt = LibUI::FFI::InitOptions.malloc
-    assert_nil LibUI.init(pt)
+    assert_nil LibUI.init
+    assert_nil LibUI.quit
+  end
+
+  def test_basic_window
+    assert_nil LibUI.init
+    assert_kind_of Fiddle::Pointer, (
+      main_window = LibUI.new_window('hello world', 300, 200, 1)
+    )
+    assert_nil LibUI.control_show(main_window)
+    assert_nil(
+      LibUI.window_on_closing(main_window) do
+        LibUI.control_destroy(main_window)
+        LibUI.quit
+        0
+      end
+    )
+    # LibUI.main
     assert_nil LibUI.quit
   end
 end
