66308729f8238f51928c0d1e6794d86ab75ab555
kojix2 <2xijok@gmail.com>
2021-03-04 17:09:30 +0900

    Add simple_notepad example

---
+++ examples/simple_notepad.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'libui'
+
+UI = LibUI
+
+UI.init
+
+main_window = UI.new_window('Notepad', 500, 300, 1)
+UI.window_on_closing(main_window) do
+  puts 'Bye Bye'
+  UI.control_destroy(main_window)
+  UI.quit
+  0
+end
+
+vbox = UI.new_vertical_box
+UI.window_set_child(main_window, vbox)
+
+entry = UI.new_non_wrapping_multiline_entry
+UI.box_append(vbox, entry, 1)
+
+UI.control_show(main_window)
+UI.main
+UI.quit
