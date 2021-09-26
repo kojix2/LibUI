7864c3b0914f64411a444e6f307cffb54d5e3514
kojix2 <2xijok@gmail.com>
2021-09-26 18:13:55 +0900

    Move fiddle monkey patch to a separate file

---
+++ lib/libui/fiddle_ext.rb
@@ -0,0 +1,69 @@
+require 'fiddle/import'
+
+module Fiddle
+  # Change the Function to hold a little more information.
+  class Function
+    # Note:
+    # Ruby 2.7 Fiddle::Function dose not have @argument_types
+    # Ruby 3.0 Fiddle::Function has @argument_types
+    attr_accessor :callback_argument_types, :argument_types
+  end
+
+  module Importer
+    def parse_signature(signature, tymap = nil)
+      tymap ||= {}
+      ctype, func, args = case compact(signature)
+                          when /^(?:[\w\*\s]+)\(\*(\w+)\((.*?)\)\)(?:\[\w*\]|\(.*?\));?$/
+                            [TYPE_VOIDP, Regexp.last_match(1), Regexp.last_match(2)]
+                          when /^([\w\*\s]+[*\s])(\w+)\((.*?)\);?$/
+                            [parse_ctype(Regexp.last_match(1).strip, tymap), Regexp.last_match(2), Regexp.last_match(3)]
+                          else
+                            raise("can't parserake the function prototype: #{signature}")
+                          end
+      symname = func
+      callback_argument_types = {}                                              # Added
+      argtype = split_arguments(args).collect.with_index do |arg, idx|          # Added with_index
+        # Check if it is a function pointer or not
+        if arg =~ /\(\*.*\)\(.*\)/                                              # Added
+          # From the arguments, create a notation that looks like a function declaration
+          # int(*f)(int *, void *) -> int f(int *, void *)
+          func_arg = arg.sub('(*', ' ').sub(')', '')                            # Added
+          # Use Fiddle's parse_signature method again.
+          callback_argument_types[idx] = parse_signature(func_arg)              # Added
+        end
+        parse_ctype(arg, tymap)
+      end
+      # Added callback_argument_types. Original method return only 3 values.
+      [symname, ctype, argtype, callback_argument_types]
+    end
+
+    def extern(signature, *opts)
+      symname, ctype, argtype, callback_argument_types = parse_signature(signature, type_alias)
+      opt = parse_bind_options(opts)
+      func = import_function(symname, ctype, argtype, opt[:call_type])
+
+      func.callback_argument_types = callback_argument_types                    # Added
+      # Ruby 2.7 Fiddle::Function dose not have @argument_types
+      # Ruby 3.0 Fiddle::Function has @argument_types
+      func.argument_types = argtype
+
+      name = symname.gsub(/@.+/, '')
+      @func_map[name] = func
+      # define_method(name){|*args,&block| f.call(*args,&block)}
+      begin
+        /^(.+?):(\d+)/ =~ caller.first
+        file = Regexp.last_match(1)
+        line = Regexp.last_match(2).to_i
+      rescue StandardError
+        file, line = __FILE__, __LINE__ + 3
+      end
+      module_eval(<<-EOS, file, line)
+        def #{name}(*args, &block)
+          @func_map['#{name}'].call(*args,&block)
+        end
+      EOS
+      module_function(name)
+      func
+    end
+  end
+end
