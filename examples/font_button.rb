0ec0c81545c5a212348c4524819ea4c51020807e
kojix2 <2xijok@gmail.com>
2021-11-18 22:38:45 +0900

    Fix a  bug

--- examples/font_button.rb
+++ examples/font_button.rb
@@ -10,7 +10,7 @@ main_window = UI.new_window('hello world', 300, 200, 1)
 
 font_button = UI.new_font_button
 font_descriptor = UI::FFI::FontDescriptor.malloc
-font_descriptor = Fiddle::RUBY_FREE
+font_descriptor.to_ptr.free = Fiddle::RUBY_FREE
 UI.font_button_on_changed(font_button) do
   UI.font_button_font(font_button, font_descriptor)
   p family: font_descriptor.Family.to_s,
