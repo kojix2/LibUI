fd870cbe4f9f1c7409302c781db14d77aa884410
kojix2 <2xijok@gmail.com>
2022-07-02 14:01:23 +0900

    Update script

--- scripts/ui_h.sh
+++ scripts/ui_h.sh
@@ -4,12 +4,12 @@ cd "$(dirname "$0")"
 
 echo "count _UI_EXTERN"
 curl -sL https://raw.githubusercontent.com/libui-ng/libui-ng/master/ui.h \
-  | sed -e "s/#.*$//g" -e "s/\/\/.*$//g" \
+  | gcc -fpreprocessed -P -dD -E - \
   | grep -o _UI_EXTERN \
   | wc -l
 
 curl -sL https://raw.githubusercontent.com/libui-ng/libui-ng/master/ui.h \
-  | sed -e "s/#.*$//g" -e "s/\/\/.*$//g" \
+  | gcc -fpreprocessed -P -dD -E - \
   | sed -z -e "s/\n//g" -e "s/;/\n/g" -e "s/\t/ /g" -e 's/  */ /g' \
   | grep "_UI_EXTERN" \
   | sed -e "s/^[ \t]*//" -e "s/[ \t]*$//" \
