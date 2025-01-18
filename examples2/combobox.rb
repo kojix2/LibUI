62629b3dc41af41c763a9a0f743b685967575677
rubyFeedback <46686565+rubyFeedback@users.noreply.github.com>
2025-01-18 11:16:09 +0000

    Create combobox.rb
    First standalone example for libui-combobox - hopefully I have not forgotten any method.

---
+++ examples2/combobox.rb
@@ -0,0 +1,97 @@
+# ============================================================================ #
+# This example (combobox.rb) shall demonstrate the following functionality
+# (9 components), as well as their implementation-status in regards to
+# this file:
+#
+#   :new_combobox                       [DONE]
+#   :combobox_append                    [DONE]
+#   :combobox_clear                     [DONE]
+#   :combobox_delete                    [DONE]          
+#   :combobox_insert_at                 [DONE]
+#   :combobox_num_items                 [DONE]
+#   :combobox_on_selected               [DONE]
+#   :combobox_selected                  [DONE]
+#   :combobox_set_selected              [DONE]
+#
+# ============================================================================ #
+require 'libui'
+LibUI.init # Initialize LibUI.
+
+# ============================================================================ #
+# === populate_the_combobox_with_this_array
+#
+# This method is used as a helper-method, to populate the combobox we use
+# here with data (an Array).
+# ============================================================================ #
+def populate_the_combobox_with_this_array(
+    the_combobox,
+    this_array
+  )
+  this_array.each {|this_entry|
+    LibUI.combobox_append(the_combobox, this_entry) # Here we add elements to the combobox.
+  }
+  LibUI.combobox_set_selected(the_combobox, 0) # The first element is now the default selected entry.
+end
+
+main_window = LibUI.new_window('combobox.rb', 400, 240, 1)
+
+hbox = LibUI.new_horizontal_box
+LibUI.box_set_padded(hbox, 1)
+
+_  = LibUI.new_combobox # Create a new combobox here.
+LibUI.box_append(hbox, _, 1) # Add the combobox here. Right now the combobox is empty.
+
+# ============================================================================ #
+# Let's add data to the combobox, as an Array:
+# ============================================================================ #
+array = %w( matz created ruby as efficient alternative to perl )
+populate_the_combobox_with_this_array(_, array)
+
+# ============================================================================ #
+# Next showing how to clear it:
+# ============================================================================ #
+LibUI.combobox_clear(_)
+populate_the_combobox_with_this_array(_, array) # And re-populate it here.
+
+# ============================================================================ #
+# Next we delete the third entry; and then insert two new elements
+# at that former position, to showcase the functionality
+# combobox_delete, as well as combobox_insert_at. Note that combobox_delete
+# takes two arguments, whereas combobox_insert_at takes three arguments.
+# ============================================================================ #
+LibUI.combobox_delete(_, 2)
+LibUI.combobox_insert_at(_, 2, 'ruby')
+LibUI.combobox_insert_at(_, 2, 'awesome')
+
+# ============================================================================ #
+# Show how many elements are in that combobox:
+# ============================================================================ #
+puts "The combobox we are using here has a total "\
+     "of #{LibUI.combobox_num_items(_)} elements."
+
+LibUI.combobox_set_selected(_, 3) # Change the selected element next, to item 4.
+
+# ============================================================================ #
+# Show the selected entry next:
+# ============================================================================ #
+puts "The presently selected entry in our combobox is element number "\
+     "#{LibUI.combobox_selected(_)}."
+
+puts 'Last but not least, try to change the combobox to a new value.'
+puts 'This will trigger LibUI.combobox_on_selected().'
+
+LibUI.combobox_on_selected(_) { |pointer|
+  selected = LibUI.combobox_selected(pointer)
+  puts "The new selection is element number `#{selected}`."
+}
+
+LibUI.window_set_child(main_window, hbox)
+LibUI.control_show(main_window)
+
+LibUI.window_on_closing(main_window) {
+  LibUI.quit
+  1
+}
+
+LibUI.main
+LibUI.uninit
